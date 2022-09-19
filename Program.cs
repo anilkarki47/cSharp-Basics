07c54dd52231dba7262b6a747f758a3175b30ae2
Anil Karki <karki.anil37@gmail.com>
2022-09-19 22:55:02 +0545

    first code

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Assignment_w1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Assignment_w1";
+            Console.ForegroundColor = ConsoleColor.Green;
+            
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
